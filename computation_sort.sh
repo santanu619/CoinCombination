ef9fae9d608ac41576e29f54402156fc1e7394e8
santanu619 <mohapatra.santanu123@gmail.com>
2021-04-08 17:31:30 +0530

    Added: Computation complete

--- computation_sort.sh
+++ computation_sort.sh
@@ -8,3 +8,11 @@ read c
 echo "First Number:$a"
 echo "Second Number:$b"
 echo "Third Number:$c"
+result1=$(($a+($b*$c)))
+result2=$((($a*$b)+$c))
+result3=$(($c+($a/$b)))
+result4=$((($a%$b)+$c))
+echo $result1
+echo $result2
+echo $result3
+echo $result4

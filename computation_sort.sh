499e54f114041d93ba2f2797227032c197e9cde4
santanu619 <mohapatra.santanu123@gmail.com>
2021-04-08 17:23:00 +0530

    Added: Code to read the data

--- computation_sort.sh
+++ computation_sort.sh
@@ -1,1 +1,10 @@
 echo "Welcome to the Program"
+echo "Enter the first Number"
+read a
+echo "Enter the second Number"
+read b
+echo "Enter the third Number"
+read c
+echo "First Number:$a"
+echo "Second Number:$b"
+echo "Third Number:$c"

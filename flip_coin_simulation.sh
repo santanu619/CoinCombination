43efcfa0f7792bd16f387fc2609c56a8bbb8b864
santanu619 <mohapatra.santanu123@gmail.com>
2021-04-10 18:51:36 +0530

    Added:code for Tossing

--- flip_coin_simulation.sh
+++ flip_coin_simulation.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 echo "Welcome to the Flip coin simulation program"
+echo "toss time"
+counter=0
+
+tails=0;
+heads=0;
+
+i=0;
+while [ true ]
+do
+
+	TossCheck=$((RANDOM%2));
+	if [ $TossCheck -eq 0 ];
+	then
+		tails=$(($tails+1));
+		echo Tails;
+	else
+		heads=$(($heads+1));
+		echo Heads;
+	fi
+done
